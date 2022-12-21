537dcecf4a47066a9e00dfefa2b85a7a5f782e69
Jhon <3227943820801@ingenieria.usac.edu.gt>
2022-12-21 15:21:01 -0600

    scanner

--- Proyecto.asm
+++ Proyecto.asm
@@ -3,7 +3,7 @@ pila segment stack       		;segmento de pila
 	     db 32 DUP('stack--')
 pila ends
 
-datos segment              		;segmento de datos, aquí van nuestras variables
+datos segment          ;segmento de datos, aquí van nuestras variables
 	msg db  '1. Ingresar ecuacion', 13, 10  ;db significa defineme un byte se interprete defineme una localidad en memoria con el nombre de msg
     msg2 db '2. Imprimir la funcion almacenada', 13, 10
     msg3 db '3. Imprimir la derivada de dicha funcion', 13, 10
@@ -13,24 +13,35 @@ datos segment              		;segmento de datos, aquí van nuestras variables
     msg7 db '7. Encontrar los ceros de la funcion por medio del metodo de Steffensen', 13, 10
     msg8 db '8. Salir de la aplicacion', 13, 10
     msg9 db '>>> Opc: $'
+    message db 13, 10, 'El numero es: $'
 datos ends
 
 codigo segment 'code'            ;segmento de codigo acá empezaremos a programar
 main proc FAR
-	       assume ss:pila, ds:datos, cs:codigo
+            assume ss:pila, ds:datos, cs:codigo
 
-	       mov    ax,datos      ; guardo en el registro ax el valor de mi seg de datos
-	       mov    ds,ax         ; lo muevo a mi datasegment (segmento de datos) para poderlos usar
+            mov    ax,datos      ; guardo en el registro ax el valor de mi seg de datos
+            mov    ds,ax         ; lo muevo a mi datasegment (segmento de datos) para poderlos usar
 
-	       mov    ah,09h         ;funcion para imprimir una cadena en pantalla
-	       ; mov dx,offset letrero   ;forma dos de imprimir una cadena en pantalla
-	       lea    dx, msg   ; le digo que me imprima letrero 
-	       int    21h           ; interrupcion 21h 
+            ;------------ Mostrar menu ------------
+            mov    ah,09h         ;funcion para imprimir una cadena en pantalla
+            lea    dx, msg   ; le digo que me imprima letrero 
+            int    21h           ; interrupcion 21h 
+            ;--------------------------------------
 
-	       mov    ah,4ch        ;funcion que finaliza un programa
-	       int    21h       
+            ;------------ Pido la opcion ------------
+            mov ah,01h
+            int 21h
+            sub al,48d ; ajusto para que apareza el numero
+            ;----------------------------------------  
 
-	       ret
+            ;Guardo la opcion en cl
+            mov cl,al
+
+            mov    ah,4ch        ;funcion que finaliza un programa
+            int    21h       
+
+            ret
 main endp                       ; fin del procedimiento main
 codigo ends                     ; find del segmento de codigo
 end main                        ; find del main
