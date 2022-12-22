ced1c7165f67dbed5b72557b51eb47b906cc6d37
Jhon <3227943820801@ingenieria.usac.edu.gt>
2022-12-22 01:17:04 -0600

    etiquetas iniciadas
    iniciadas para hacer bucle

--- Proyecto.asm
+++ Proyecto.asm
@@ -24,24 +24,65 @@ main proc FAR
             mov    ds,ax         ; lo muevo a mi datasegment (segmento de datos) para poderlos usar
 
             ;------------ Mostrar menu ------------
-            mov    ah,09h         ;funcion para imprimir una cadena en pantalla
-            lea    dx, msg   ; le digo que me imprima letrero 
-            int    21h           ; interrupcion 21h 
+            call mostrar_menu
             ;--------------------------------------
 
-            ;------------ Pido la opcion ------------
-            mov ah,01h
-            int 21h
-            sub al,48d ; ajusto para que apareza el numero
-            ;----------------------------------------  
-
-            ;Guardo la opcion en cl
-            mov cl,al
+            call limpiar
+            call posCursor
+            call mostrar_menu
 
             mov    ah,4ch        ;funcion que finaliza un programa
             int    21h       
 
             ret
 main endp                       ; fin del procedimiento main
+
+
+mostrar_menu proc
+
+    ;------------ Mostrar menu ------------
+    mov    ah,09h         ;funcion para imprimir una cadena en pantalla
+    lea    dx, msg   ; le digo que me imprima letrero 
+    int    21h           ; interrupcion 21h 
+    ;--------------------------------------
+
+    ;------------ Pido la opcion ------------
+    mov ah,01h
+    int 21h
+    sub al,48d ; ajusto para que aparezca el numero
+    ;----------------------------------------  
+
+    ;Guardo la opcion en cl
+    mov cl,al
+    ret
+
+mostrar_menu endp
+
+
+limpiar proc
+
+    mov ah, 06h
+    mov al, 00h
+    mov bh, 07h
+    mov cx, 0000h
+    mov dx, 184fh
+    int 10h
+
+    ret
+
+limpiar endp
+
+posCursor proc
+
+    mov ah,02h      ; funcion para posicionar el cursor en la pantalla
+    mov bh,00h      ; modo de video
+    mov dh,00h      ; columna donde se pondra
+    mov dl,00h      ; fila donde se pondra
+    int 10h
+
+    ret
+
+posCursor endp
+
 codigo ends                     ; find del segmento de codigo
 end main                        ; find del main
